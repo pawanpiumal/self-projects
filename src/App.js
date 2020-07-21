2bc4b6de8194427773b8baf84a855eaa27dd73f1
pawanpiumal <pawanpiumal1@gmail.com>
2020-07-22 01:09:22 +0530

    axios test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
+import axios from 'axios'
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      msg:"Loading"
+    }
+  }
+  
+  get=()=>{
+    axios.get("https://sheltered-cliffs-07425.herokuapp.com/").then(data=>{
+      this.setState({
+        msg:data.data.msg
+      });
+    })
+  }
+  componentDidMount=()=>{
+    this.get();
+  }
+  render() {
+    return (
+      <div>
+        {this.state.msg}
+      </div>
+    );
+  }
 }
 
 export default App;

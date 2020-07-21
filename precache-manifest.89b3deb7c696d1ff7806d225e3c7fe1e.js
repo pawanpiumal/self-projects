27b283c7fc6643a084239b129348f8d5fa2061e5
pawanpiumal <pawanpiumal1@gmail.com>
2020-07-22 01:03:31 +0530

    Updates

---
+++ precache-manifest.89b3deb7c696d1ff7806d225e3c7fe1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376aa333bcf661b6567e7b1980f07d0a",
+    "url": "/self-projects/index.html"
+  },
+  {
+    "revision": "e8850b44ae46e2142e96",
+    "url": "/self-projects/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1573c881f68cfa37d69d",
+    "url": "/self-projects/static/js/2.2aa347af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/self-projects/static/js/2.2aa347af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8850b44ae46e2142e96",
+    "url": "/self-projects/static/js/main.57f02511.chunk.js"
+  },
+  {
+    "revision": "637b43e0ff25d10a706f",
+    "url": "/self-projects/static/js/runtime-main.8eb38170.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/self-projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
